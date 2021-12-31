819b1064ad8dec3dd886e4dea0de6a32842fbb09
Dinesh kumar T P <1da18cs046.cs@drait.edu.in>
2021-12-31 16:02:54 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+        <div class="header_main">
+		<div class="navBar">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php#aboutus">About Us</a></li>
+                <li><a href="index.php#contactus">Contact Us</a></li>
+				<li><a href="staff_login.php">Staff Login</a></li>
+			
+			</ul>
+		</div>
+	 <a href="index.php"><div class="logo-name">
+			<div class="logo">
+             <img class="logo_img" src="img/bank2.png">
+			</div>
+			
+			<div class="name">
+				<h5> Online Banking System</h5></a><br>
+			</div>
+        </div>
+            
+            
+            <div class="dif_banking">
+			<div class="retail_banking">
+				<a href="customer_reg_form.php"> Open Account </a>
+			</div>
+			<div class="corporate_banking">
+				<a href="debit_card_form.php"> Apply For Debit Card </a>
+			</div>
+			<div class="international_banking">
+				<a href="fund_transfer.php"> Fund Transfer </a>
+			</div>
+			<div class="international_banking">
+				<a href="index.php#contactus"> Contact Us </a>
+			</div>
+                
+            <div class="bank_servic">
+				<a href="index.php#disclaimerr"> Disclaimer </a>
+			</div>
+		</div>
+            
+           
+			
+	</div>
---
+++ index.php
@@ -0,0 +1,65 @@
+<html>
+<title>Online Banking System</title>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/index.css">
+   <link rel="shortcut icon" href="img/chase.jpg">
+    </head>
+ 
+<body>
+<?php include'header.php'?>
+<div class="index_container">
+    <div class="slider">
+    <div class="slideimg">
+    <img src="img/back-1.png">
+        <img src="img/back-2.png">
+        <img src="img/back-3.png">
+        <img src="img/back-4.png">
+        <img src="img/onbnk.jpg">
+        </div>
+    </div><br>
+    <!-- <div class="newsroom">
+        
+        <marquee class="marquee_news" scrolldelay="20"><p class="newsfeed"><span>Your bank may charge you for closing a bank account. 
+        Bank also charges you when you close your account within a particular time period.</span><span>SBI cuts deposit rates; PPF to fetch lower interest rate. </span><span>No, it is not mandatory to link your bank account with CITIZENSHIP card</span></p></marquee>
+    </div><br><br> -->
+   
+    
+
+<div class="online_services">
+    <h3>Online Services</h3>
+        <ul>
+            <a href="customer_reg_form.php"><li>Open Account</li></a>
+            <a href="debit_card_form.php"><li>Apply For Debit Card</li></a><br>
+            <a href="#" id="ebanking" ><li><div class="ebanking">Internet Banking
+                <div class = "ebanking_options">
+                <ul>
+                    <a href="customer_login.php"><li>Login </li></a>
+                    <a href="ebanking_reg_form.php"><li>Register</li></a>
+                </ul>
+            </div>
+        </div>
+    </li></a>
+            <a href="fund_transfer.php"><li>Fund Transfer</li></a><br>
+        </ul>
+   
+</div>
+
+
+        <div id="aboutus" class="about"><span>About Us</span><br><br>
+        <p>We use our global capabilities and deep local knowledge in India to provide a wide range of products and services to meet the needs of our individual and business clients. We build our products and services around our clients and have a commitment to help ensure that our clients have the best possible experience with us.Our footprint connects emerging and high-growth markets with more established economies, allowing us to channel capital to where it’s needed the most.We’re determined to develop significant sustainable finance revenues to grow our business, and to integrate environmental and social considerations into all our decision-making, so that we can advance our ambition to become the world’s most sustainable and responsible bank. We’re in a unique position to support the massive shift of capital towards sustainable finance, which has become a priority for investors, companies and individuals alike.
+</p></div>
+    
+    <div id="disclaimerr" class="disclaimer">
+    <span>Disclaimer </spasn><br><br>
+        <p>Certain financial products and services are required by law to be provided only by licensed representatives and affiliates. Inquiries regarding such products and services will be referred to a licensed representative or a licensed affiliate.</p>
+        <p>Our bank does not ask for the details of your account/PIN/password. Therefore any one pretending to be asking you for information from the bank/technical team may be fraudulent entities, so please beware.</p>
+        <p>We shall also not be responsible for wrong transactions and wanton disclosure of details by you. Viewing option and transaction option on the net are different. You may exercise your option diligently.</p>
+    </div>
+
+
+    </div>
+    
+<?php include 'footer.php';?>
+</body>
+
+</html>

8d9cde9e1218f71658cbba24954c35985c7ea636
Dinesh kumar T P <1da18cs046.cs@drait.edu.in>
2022-01-01 10:29:49 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,54 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer>
+<div class="container">
+<div class="follow_us">
+    <label>Follow Us</label>
+    <ul>
+        <li><a href="https://www.facebook.com/">Facebook</a></li>
+        <li><a href="https://twitter.com/">Twitter</a></li>
+        <li><a href="https://www.instagram.com/">Instagram</a></li>
+    
+    </ul>
+    </div>
+    <div class="contact" id="contactus">
+        <label>Contact Us</label>
+        <ul>
+            <p>112 5th Block Koramangala ,</p><p>Bengaluru, Karnataka 560095</p>
+            <p>Tel : 9871234565</p>
+            <p> Email: contact@obs</p>
+    
+        </ul>
+    </div>
+        <div class="links">
+            <label>Important Links</label>
+            <ul>    
+            <li><a href="index.php#aboutus" target="_blank">About Us</a></li>
+            <li><a href="https://www.bankbazaar.com/credit-score.html">Loan CreditScore</a></li>
+            <li><a href="https://emicalculator.net/">EMI Calculator</a></li> 
+            </ul><br>
+    </div>
+    <div>
+        <div class="chatbot">
+            <label >Help Bot</label>
+            <ul>
+                <li>
+                <iframe src='https://webchat.botframework.com/embed/bankingqna-9486-bot?s=g0sS4mEyQtc.3NsqqUxAm7jUadiFdn5E84xnA21zgMrlGdcRSWy6Vuk'  style='min-width: 50px; min-height: 250px;'></iframe>
+                </li>
+            </ul>
+    </div>
+    
+
+    
+    
+    
+    </div>
+    <div class="copyright">
+        <span>Copyright &copy; 2021-22 Online Banking System. All rights reserved.</span>
+    </div>
+    
+    <div class="bestview">
+    <span>Site best viewed at 1024 x 768 resolution in Internet Explorer 10+, Google Chrome 49+, Firefox 45+ and Safari 6+</span>
+    
+    </div>
+
+</footer>
